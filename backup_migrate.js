8b0e07266b5be8e39574bf1ee9b27a2172634756
Ronan Dowling <ronan@gortonstudios.com>
2009-11-18 17:51:09 +0000

    Fixed. Fatal error while backing up to database destination Fixed. JS error on simple backup page, switch dbs to checkboxes fix.

--- backup_migrate.js
+++ backup_migrate.js
@@ -3,24 +3,26 @@
 Drupal.backup_migrate = {
   callbackURL : "",  
   autoAttach  : function() {
-    if ($("#edit-save-settings").length && !$("#edit-save-settings").attr("checked")) {
-      // Disable input and hide its description.
-      // Set display none instead of using hide(), because hide() doesn't work when parent is hidden.
-      $('div.backup-migrate-save-options').css('display', 'none');
+    if (Drupal.settings.backup_migrate !== undefined) {
+      if ($("#edit-save-settings").length && !$("#edit-save-settings").attr("checked")) {
+        // Disable input and hide its description.
+        // Set display none instead of using hide(), because hide() doesn't work when parent is hidden.
+        $('div.backup-migrate-save-options').css('display', 'none');
+      }
+  
+      $("#edit-save-settings").bind("click", function() {
+        if (!$("#edit-save-settings").attr("checked")) {
+          $("div.backup-migrate-save-options").slideUp('slow');
+        }
+        else {
+          // Save unchecked; enable input.
+          $("div.backup-migrate-save-options").slideDown('slow');
+        }
+      });
+  
+      $('#edit-filters-exclude-tables').after('<div class="description backup-migrate-checkbox-link"><a href="javascript:Drupal.backup_migrate.selectToCheckboxes(\''+ 'exclude_tables' +'\');">'+ Drupal.settings.backup_migrate.checkboxLinkText +'</a></div>');
+      $('#edit-filters-nodata-tables').after('<div class="description backup-migrate-checkbox-link"><a href="javascript:Drupal.backup_migrate.selectToCheckboxes(\''+ 'nodata_tables' +'\');">'+ Drupal.settings.backup_migrate.checkboxLinkText +'</a></div>');
     }
-
-    $("#edit-save-settings").bind("click", function() {
-      if (!$("#edit-save-settings").attr("checked")) {
-        $("div.backup-migrate-save-options").slideUp('slow');
-      }
-      else {
-        // Save unchecked; enable input.
-        $("div.backup-migrate-save-options").slideDown('slow');
-      }
-    });
-
-    $('#edit-exclude-tables').after('<div class="description backup-migrate-checkbox-link"><a href="javascript:Drupal.backup_migrate.selectToCheckboxes(\''+ 'exclude_tables' +'\');">'+ Drupal.settings.backup_migrate.checkboxLinkText +'</a></div>');
-    $('#edit-nodata-tables').after('<div class="description backup-migrate-checkbox-link"><a href="javascript:Drupal.backup_migrate.selectToCheckboxes(\''+ 'nodata_tables' +'\');">'+ Drupal.settings.backup_migrate.checkboxLinkText +'</a></div>');
   },
 
   processCheckboxes : function(ctxt) {
@@ -39,7 +41,7 @@ Drupal.backup_migrate = {
   },
 
   selectToCheckboxes : function(field) {
-    var field_id = 'edit-'+ field.replace('_', '-');
+    var field_id = 'edit-filters-'+ field.replace('_', '-') ;
     var $select = $('#'+ field_id);
     var $checkboxes = $('<div></div>').addClass('backup-migrate-tables-checkboxes');
     $('option', $select).each(function(i) {
