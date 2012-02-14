2877632f691be7865b0b2d27c65eba445cf9edb4
Ronan Dowling <ronan@gortonstudios.com>
2012-02-14 00:28:49 -0600

    Added more dependent fields to the UI

--- backup_migrate.js
+++ backup_migrate.js
@@ -2,23 +2,38 @@
 Drupal.backup_migrate = {
   callbackURL : "",  
   autoAttach  : function() {
-    if (Drupal.settings.backup_migrate !== undefined) {
-      if ($("#edit-save-settings").length && !$("#edit-save-settings").attr("checked")) {
-        // Disable input and hide its description.
-        // Set display none instead of using hide(), because hide() doesn't work when parent is hidden.
-        $('div.backup-migrate-save-options').css('display', 'none');
+    if (Drupal.settings.backup_migrate !== undefined) { 
+      if (Drupal.settings.backup_migrate.dependents !== undefined) { 
+        for (key in Drupal.settings.backup_migrate.dependents) {
+          info = Drupal.settings.backup_migrate.dependents[key];
+          dependent = $('#edit-' + info['dependent']);
+          for (key in info['dependencies']) {
+            dependency = $('#edit-' + key);
+            dependency.checkval = function(inval) {
+              var val = this.val();
+              if (this.attr('type') == 'checkbox') {
+                val = this.attr('checked');
+              }
+              return val == inval;
+            };
+
+            if (!dependency.checkval(info['dependencies'][key])) {
+              dependent.css('display', 'none');
+            }
+            (function(dependent, dependency) {
+              dependency.bind('change click keypress focus', function() {
+                if (dependency.checkval(info['dependencies'][key])) {
+                  dependent.slideDown();
+                }
+                else {
+                  dependent.slideUp();
+                }
+              });
+            })(dependent, dependency);
+          }
+        }
       }
-  
-      $("#edit-save-settings").bind("click", function() {
-        if (!$("#edit-save-settings").attr("checked")) {
-          $("div.backup-migrate-save-options").slideUp('slow');
-        }
-        else {
-          // Save unchecked; enable input.
-          $("div.backup-migrate-save-options").slideDown('slow');
-        }
-      });
-  
+
       $('#edit-filters-exclude-tables').after('<div class="description backup-migrate-checkbox-link"><a href="javascript:Drupal.backup_migrate.selectToCheckboxes(\''+ 'exclude_tables' +'\');">'+ Drupal.settings.backup_migrate.checkboxLinkText +'</a></div>');
       $('#edit-filters-nodata-tables').after('<div class="description backup-migrate-checkbox-link"><a href="javascript:Drupal.backup_migrate.selectToCheckboxes(\''+ 'nodata_tables' +'\');">'+ Drupal.settings.backup_migrate.checkboxLinkText +'</a></div>');
     }
